c69a99e3f38f182b8f703d3fa3e52223c2bef155
John Smith <vasilda32@yandex.ru>
2023-07-12 19:07:24 +0300

    + метод execute() класса Db

--- App/Db.php
+++ App/Db.php
@@ -4,8 +4,14 @@ namespace App;
 
 class Db
 {
+    protected $dbh;
     public function __construct()
     {
-        echo 'Hello, DB!';
+        $this->dbh = new \PDO('mysql:host=127.0.0.1;dbname=test', 'root', '');
+    }
+
+    public function execute($sql)
+    {
+        return $this->dbh->prepare($sql)->execute();
     }
 }
